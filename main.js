38aa08ecea095d19d9e9c3adaeeabe1b34831aee
prabhjot singh <48572962+prabh234@users.noreply.github.com>
2023-03-18 18:33:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ colapse.on('click',()=>{
         navbar.attr('data-visible','true');
         colapse.attr('aria-expanded','true');
         colapse.css('background-image','url(assets/images/icon-menu-close.svg)');
+        navbar.css('box-shadow',' 0 0 0 200vmax #00000050')
     }
     else{
         navbar.css('transform','translateX(100%)')
         navbar.attr('data-visible','false'); 
         colapse.attr('aria-expanded','false');
         colapse.css('background-image','url(assets/images/icon-menu.svg)');   
-        
+        navbar.css('box-shadow','none')
     }
   
 })
